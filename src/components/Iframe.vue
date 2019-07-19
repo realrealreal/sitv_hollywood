835578d7dd42597d49e654f136d47f22ae8575fa
shanjing <369473521@qq.com>
2019-07-19 18:14:39 +0800

    iframe 页面

---
+++ src/components/Iframe.vue
@@ -0,0 +1,18 @@
+<template>	
+	<iframe name="myframe" :src="src" frameborder="0" width="100%" height="100%"></iframe>
+</template>
+
+<script>
+export default {
+  name: 'Iframe',
+  data () {
+    return {
+    	src:'https://www.baidu.com'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
