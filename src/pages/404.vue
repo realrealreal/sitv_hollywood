3cc78d096fef9beaa02a47c4c4f788ca0de73a61
shanjing <369473521@qq.com>
2019-07-19 18:14:18 +0800

    404 页面

---
+++ src/pages/404.vue
@@ -0,0 +1,23 @@
+<template>	
+	<div>404</div>
+</template>
+
+<script>
+export default {
+  name: '404',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='stylus'>
+div
+  color: #fff;
+  text-align: center;
+  height: 720px;
+  line-height: 720px;
+  font-size: 600px;
+</style>
