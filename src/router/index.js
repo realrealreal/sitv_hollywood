0c9cce431a31c36424c337f1bcf3df4c53c635b6
shanjing <369473521@qq.com>
2019-07-31 18:46:06 +0800

    路由 更新2019/07/31

--- src/router/index.js
+++ src/router/index.js
@@ -1,49 +1,71 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+//import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 let routes = [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      //name: 'HelloWorld',
+      //component: HelloWorld,
+      redirect: { path: '/hollywood/index' }
     },
     {
       path: '/hollywood/index',
-      name: 'HollyWood',
-      component: () => import('@/pages/index/index') // 懒加载引入方式
+      name: 'Index',
+      component: () => import('@/pages/index/index'), // 懒加载引入方式
+      meta: {
+        keepAlive: false // 不需要被缓存
+      }
     },
     {
       path: '/test',
       name: 'Test',
-      component: () => import('@/pages/test') // 懒加载引入方式
+      component: () => import('@/pages/test'), // 懒加载引入方式
+      meta: {
+        keepAlive: false // 不需要被缓存
+      }
     },
     {
       path: '/hollywood/repo',
       name: 'Repo',
-      component: () => import('@/pages/index/personal') // 懒加载引入方式
+      component: () => import('@/pages/index/personal'), // 懒加载引入方式
+      meta: {
+        keepAlive: false // 不需要被缓存
+      }
     },
     {
       path: '/hollywood/repo/orderlist',
       name: 'OrderList',
-      component: () => import('@/pages/list/orderList') // 懒加载引入方式
+      component: () => import('@/pages/list/orderList'), // 懒加载引入方式
+      meta: {
+        keepAlive: false // 不需要被缓存
+      }
     },
     {
       path: '/hollywood/repo/:name',
       name: 'RepoList',
-      component: () => import('@/pages/list/repolist') // 懒加载引入方式
+      component: () => import('@/pages/list/repolist'),// 懒加载引入方式
+      meta: {
+        keepAlive: false // 不需要被缓存
+      }
     },
     {
       path: '/hollywood/movielist',
       name: 'Movielist',
-      component: () => import('@/pages/list/list') // 懒加载引入方式
+      component: () => import('@/pages/list/list'), // 懒加载引入方式
+      meta: {
+        keepAlive: true // 需要被缓存
+      }
     },
     {
       path: '/hollywood/detail',
       name: 'Details',
-      component: () => import('@/pages/detail/vod') // 懒加载引入方式
+      component: () => import('@/pages/detail/vod'), // 懒加载引入方式
+      meta: {
+        keepAlive: false // 不需要被缓存
+      }
     },
     {
       path: '/hollywood',
@@ -52,13 +74,18 @@ let routes = [
     {
       path: '/hollywood/iframe',
       name: 'Iframe',
-      component: () => import('@/components/Iframe')
+      component: () => import('@/components/Iframe'),
+      meta: {
+        keepAlive: true // 需要被缓存
+      }
     },
     {
       path: '/404',
       name: '404',
-      component: () => import('@/pages/404')
-      
+      component: () => import('@/pages/404'),
+      meta: {
+        keepAlive: true // 需要被缓存
+      }
     },
     {
       path: '*',
@@ -68,6 +95,7 @@ let routes = [
 
 // 全局路由钩子
 const router = new Router({
+  //mode: 'history',  默认hash模式
   routes
 })
 
@@ -75,6 +103,13 @@ router.beforeEach((to, from, next) => {
   console.log('路由钩子')
   console.log(to)
   console.log(from)
+  if(to.name == 'Movielist'){
+    if(from.name == 'Details'){
+      to.meta.isBack = true;
+    }else{
+      to.meta.isBack = false;
+    } 
+  }
   next()
 })
 
