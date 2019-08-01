04c93936706f7e23ef0b0748f2cdc23d67b7cbce
shanjing <369473521@qq.com>
2019-08-01 17:57:31 +0800

    接口地址配置文件 20190801

--- src/api/url-config.js
+++ src/api/url-config.js
@@ -6,6 +6,8 @@ export default {
   movieIndexApi: `${base}epg/ott/movie.main`,
   queryHistoryApi: `${base}epg/api/tvportal/queryHistory.json`,
   queryCollectionApi: `${base}epg/api/tvportal/queryCollection.json`,
+  addCollectionApi: `${base}epg/api/tvportal/addCollection.json`,
+  deleteCollectionApi: `${base}epg/api/tvportal/deleteCollection.json`,
   searchApi: `${base}epg/api/search.json`, //http://localhost:8999/epg/api/search.json?action=byTag&condition=美国,其他,好莱坞,改编,奥斯卡&curPage=1&pageSize=50&mainFolder=电影&hdType=1
   bizListApi: `${base}epg/api/bizlist/`,
   categroyListApi: `${base}epg/api/categoryitem/`
